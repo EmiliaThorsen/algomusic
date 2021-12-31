e9df4e1ca67898f9331e2235d79205623fff4faa
EmiliaThorsen <samuel.j.thors@gmail.com>
2021-12-31 19:35:58 +0100

    impiment primitive mixer

--- src/main.c
+++ src/main.c
@@ -8,30 +8,40 @@
 #define BUFFER_SIZE (512)
 
 
+//the data structure for the calculated audio track data
 typedef struct {
-    int trackID;
-    float trackLevel;
-    float *data;
+    int trackID; //id of the track
+    float trackLevel; //the mixer level of the track
+    float *data; //audio data of the track
 } TrackSoundData;
 
 
+//the data passed into pacallback
 typedef struct {
-    TrackSoundData *tracks;
-    unsigned long int frame;
+    TrackSoundData *data; //array of audio data tracks
+    unsigned long int frame; //the frame in the audio tracks the player is in
+    int tracks; //the amount of tracks the mixer has to combine
 } paCallbackData;
 
 
 int pacallback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData){
     paCallbackData *data = (paCallbackData*)userData;  
     float *out = (float*)outputBuffer;
-    unsigned int i;
     
-    for(i=0; i<framesPerBuffer; i++) {
-    data->frame++;
-    *out++ = data->tracks->data[data->frame];
-    *out++ = data->tracks->data[data->frame];
+    for(int i=0; i<framesPerBuffer; i++) {
+        data->frame++;
+
+        float leftChanel = 0.0;
+        float rightChanel = 0.0;
+        for(int track=0; track<data->tracks; track++){
+            leftChanel += data->data[track].data[data->frame]*data->data[track].trackLevel;
+            rightChanel += data->data[track].data[data->frame]*data->data[track].trackLevel;
+        }
+
+        *out++ = leftChanel/data->tracks;
+        *out++ = rightChanel/data->tracks;
    }
-    return 0;
+   return 0;
 }
 
 
@@ -45,17 +55,26 @@ int main () {
     
     float frame = 0.0;
     float track0data[220500];
+    float track1data[220500];
     for (int i=0; i<220500; i++) {
         frame++;
-        track0data[i] = sin((frame/44100)*800);
+        track0data[i] = sin((frame/44100)*1600);
+        track1data[i] = sin((frame/44100)*3200);
     }
-       
-    TrackSoundData track = {1, 1.0, track0data};
-    paCallbackData data; 
-    data.tracks = &track;
 
-    PaStream *stream;
+    TrackSoundData track0 = {0, 1.0, track0data};
+    TrackSoundData track1 = {1, 1.0, track1data};
+
+    TrackSoundData tracks[2];
+    tracks[0] = track0;
+    tracks[1] = track1;
+
+    paCallbackData data; 
+    data.data = tracks;
+    data.tracks = 2;
+
     //intitiates portaudio stream
+    PaStream *stream;
     err = Pa_OpenDefaultStream(&stream, 0, 2, paFloat32, SAMPLE_RATE, BUFFER_SIZE, pacallback, &data);
     if(err != paNoError) goto error;
     printf("stream initiated!\n");
